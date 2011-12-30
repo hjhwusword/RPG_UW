b4ed45784523f9314e6126c0e7f02d0b3c4ed24b
Chieh-Han Wu <chiehwu@u.washington.edu>
2011-12-30 15:10:03 -0800

    Update MusicMidi and MusicOgg

--- src/Music/MusicMidi.java
+++ src/Music/MusicMidi.java
@@ -1,6 +1,8 @@
 package Music;
 
 public enum MusicMidi implements MusicType {
+	// TODO finish the list according to our audio files
+	// the list of midi
 	MIDITEST,
 	MIDITEST2;
 }
--- src/Music/MusicOgg.java
+++ src/Music/MusicOgg.java
@@ -1,6 +1,8 @@
 package Music;
 
 public enum MusicOgg implements MusicType {
+	// TODO finish the list according to our audio files
+	// stores the list of ogg
 	OGGTEST,
 	OGGTEST2;
 }
